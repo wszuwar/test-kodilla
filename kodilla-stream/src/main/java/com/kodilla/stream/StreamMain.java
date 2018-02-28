28a7978eea1fe013ce9118e87f5f6e7d134ef75d
“wszuwar <msuwr315@gmail.com>
2018-02-28 22:51:28 +0100

    task 7.1

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,36 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+
+        System.out.println("Calculating expressions with lambdas");
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        expressionExecutor.executeExpression(10,5, (a,b) -> a + b);
+        expressionExecutor.executeExpression(10,5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10,5,(a, b) -> a * b);
+        expressionExecutor.executeExpression(10,5,(a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3,4,FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Piekny text",(text) -> text.toUpperCase());
+        poemBeautifier.beautify("Wspaniale",(text ->  String.valueOf(new StringBuilder("ABC ").append(text.concat(" ABC")))));
+        poemBeautifier.beautify("Jestes Boski",(text -> text.replace('e','3').replace('o','0').replace('a','@')));
+        poemBeautifier.beautify("Browar",(text -> String.valueOf(new StringBuilder(text).reverse())));
+
+
+
+
+
+
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/lambda/MathExpresion.java
@@ -0,0 +1,5 @@
+package com.kodilla.stream.lambda;
+
+public interface MathExpresion {
+    double calculateExpression(double a, double b);
+}
