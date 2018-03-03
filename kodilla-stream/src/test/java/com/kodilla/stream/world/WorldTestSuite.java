7fb14a8b7c0b9a47ab349683f47fc720aff16193
“wszuwar <msuwr315@gmail.com>
2018-03-03 15:16:07 +0100

    Remake task 7.4

--- kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -12,20 +12,20 @@ public class WorldTestSuite {
     public void testGetPeopleQuantity(){
         //Given
         World world = new World();
-        Continent Europe = new Continent();
-        Europe.addCountry(new Country("Poland",new BigDecimal("38000000")));
-        Europe.addCountry(new Country("Denmark",new BigDecimal("45000000")));
-        Europe.addCountry(new Country("Germany",new BigDecimal("20000000")));
-        Europe.addCountry(new Country("Spain",new BigDecimal("38000000")));
-        Continent Asia = new Continent();
-        Asia.addCountry(new Country("Japan",new BigDecimal("25000000")));
-        Asia.addCountry(new Country("China",new BigDecimal("594000000")));
-        Continent NorthAmerica = new Continent();
-        NorthAmerica.addCountry(new Country("USA",new BigDecimal("650000000")));
-        NorthAmerica.addCountry(new Country("Canada",new BigDecimal("421000000")));
-        world.addContinent(Europe);
-        world.addContinent(Asia);
-        world.addContinent(NorthAmerica);
+        Continent europe = new Continent();
+        europe.addCountry(new Country("Poland",new BigDecimal("38000000")));
+        europe.addCountry(new Country("Denmark",new BigDecimal("45000000")));
+        europe.addCountry(new Country("Germany",new BigDecimal("20000000")));
+        europe.addCountry(new Country("Spain",new BigDecimal("38000000")));
+        Continent asia = new Continent();
+        asia.addCountry(new Country("Japan",new BigDecimal("25000000")));
+        asia.addCountry(new Country("China",new BigDecimal("594000000")));
+        Continent northAmerica = new Continent();
+        northAmerica.addCountry(new Country("USA",new BigDecimal("650000000")));
+        northAmerica.addCountry(new Country("Canada",new BigDecimal("421000000")));
+        world.addContinent(europe);
+        world.addContinent(asia);
+        world.addContinent(northAmerica);
         System.out.println(world.getPeopleQuantity().unscaledValue());
         //When
         BigDecimal totalQuantityOfPpl = new BigDecimal("1831000000");
