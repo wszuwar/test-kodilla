5ebaaa8d82622095238ce53d540f0409994dd50f
“wszuwar <msuwr315@gmail.com>
2018-03-09 01:22:28 +0100

    task8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -8,7 +8,7 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileReader {
-    public void readFile(){
+    public void readFile() throws FileReaderException {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("File/names.txt").getFile());
 
@@ -17,7 +17,7 @@ public class FileReader {
             Stream<String> fileLines = Files.lines(Paths.get(file.getPath()));
             fileLines.forEach(System.out::println);
         } catch (IOException e) {
-            System.out.println("Oh no something went wrong!" + e);
+            throw new FileReaderException();
         }
         finally {
             System.out.println("Im gona be here... always");
