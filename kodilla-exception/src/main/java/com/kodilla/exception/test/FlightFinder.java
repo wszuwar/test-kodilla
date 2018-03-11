03a4cb1e9b4cd0a74b6e300e58452e1b4ff29675
“wszuwar <msuwr315@gmail.com>
2018-03-11 22:11:59 +0100

    8.4 final v.

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -7,12 +7,12 @@ import java.util.stream.Collectors;
 public class FlightFinder {
     public void findFlight(Flight flight) throws RouteNotFoundException {
         HashMap<String,Boolean> flightRoute = new HashMap<>();
-        flightRoute.put("Cracow",false);
+        flightRoute.put("Cracow",true);
         flightRoute.put("Stavanger",true);
         flightRoute.put("Oslo",true);
         flightRoute.put("Warsaw",true);
 
-       if ( flightRoute.containsKey(flight.getDepartureAirport())&& flightRoute.get(flight.getDepartureAirport()).equals(true) ){
+       if ( flightRoute.containsKey(flight.getDepartureAirport())&& flightRoute.get(flight.getDepartureAirport()) ){
              System.out.println("Your Flight is avaible");
        }else  {
            throw new RouteNotFoundException("Your flight is unavaible");
